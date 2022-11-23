e58ec2b9dbbb54ad00757549cbde9186f96603e2
Lone Detective <45597734+aka-nahal@users.noreply.github.com>
2022-11-23 07:04:18 +0000

    yes i did it

---
+++ praticial-mst.c
@@ -0,0 +1,26 @@
+
+#include<stdio.h>
+
+int main(){
+
+    int a;
+    
+
+    printf("Enter Any Number:- ");
+    scanf("%d" ,&a);
+  
+ 
+
+    if (a&1&a){
+        printf("%d is Odd \n",a);
+
+    }
+    else{
+
+        printf("%d is Even \n",a);
+ }
+
+    return 0;
+}
+
+
